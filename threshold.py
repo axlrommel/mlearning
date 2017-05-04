740a16193b2b6a93b7c3ad4b5443e2f408195abf
ravill2 <ravill2@monsanto.com>
2017-05-04 16:30:19 -0500

    changed the image to bit 1 bit

--- threshold.py
+++ threshold.py
@@ -2,35 +2,28 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-def threshold(imageArray):
-    balanceAr = []
-    newAr = imageArray
-    for eachRow in imageArray:
-        for eachPix in eachRow:
-            avgNum = reduce(lambda x, y: x + y, eachPix[:2]) / len(eachPix[:2])
-            balanceAr.append(avgNum)
-    balance = reduce(lambda x, y: x + y, balanceAr) / len(balanceAr)
-    for eachRow in newAr:
-        for eachPix in eachRow:
-            if reduce(lambda x, y: x + y, eachPix[:2]) / len(eachPix[:2]) > balance:
-                eachPix[0] = 255
-                eachPix[1] = 255
-                eachPix[2] = 255
-                #eachPix[3] = 255
-            else:
-                eachPix[0] = 0
-                eachPix[1] = 0
-                eachPix[2] = 0
-                #eachPix[3] = 255
-    return newAr
+def threshold(image_array):
+    """Returns a 0 if a pixel is [0,0,0] and 1 otherwise."""
+    numrows = len(image_array)
+    numcols = len(image_array[0])
+    newar = [[0 for x in range(numrows)] for y in range(numcols)]
+
+    i = 0
+    for each_row in image_array:
+        j = 0
+        for each_pix in each_row:
+            if each_pix[0] != 0 or each_pix[1] != 0 or each_pix[2] != 0:
+                newar[i][j] = 1
+            j = j + 1
+        i = i + 1
+    return newar
 
 im = Image.open(
     "/Users/ravill2/birdPhotos/CUB_200_2011/CUB_200_2011/backInBlack/011.Rusty_Blackbird/Rusty_Blackbird_0016_6684.jpg")
-im = im.resize((64,64),Image.NEAREST)
+im = im.resize((32,32),Image.NEAREST)
 im = im.convert("RGB")
-#im = im.convert("1")
 iar = np.array(im)
 iar = threshold(iar)
-plt.imshow(iar)
 print(iar)
+plt.imshow(iar)
 plt.show()
